5146352fe4f56b155b20a87ce2e016255afe8658
FrederikGJ <113090989+FrederikGJ@users.noreply.github.com>
2024-03-11 08:00:02 +0100

    update

--- pages/machine_learning.py
+++ pages/machine_learning.py
@@ -1,6 +1,13 @@
 import streamlit as st
+import pandas as pd
 
 def show():
     st.title("Machine learning")
 
     ## i need to do some machine learning with my suicide data the json files in perticular 
+
+    # Read the JSON file into a DataFrame
+    data = pd.read_json('/data/collection_sui_notes.json')
+
+    # Display the DataFrame
+    st.write(data)
