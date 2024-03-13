29b2304ecc8f150f5dbe6e53ed5abd4e06dd2870
Natasja Vitoft <natasjavitoft@outlook.dk>
2024-03-13 16:30:01 +0100

    Natasja fine branch

--- pages/machine_learning.py
+++ pages/machine_learning.py
@@ -102,12 +102,12 @@ def show():
 
 def merge_json_to_txt(data_folder):
     txt_file_path = os.path.join(data_folder, "merged_data.txt")
-    with open(txt_file_path, 'w') as txt_file:
+    with open(txt_file_path, 'w', encoding='utf-8') as txt_file:
         for filename in os.listdir(data_folder):
             if filename.endswith('.json'):
                 filepath = os.path.join(data_folder, filename)
                 try:
-                    with open(filepath, 'r') as f:
+                    with open(filepath, 'r', encoding='utf-8') as f:
                         json_data = json.load(f)
                         json_str = json.dumps(json_data, indent=4)
                         txt_file.write(json_str + "\n\n")
@@ -115,7 +115,7 @@ def merge_json_to_txt(data_folder):
                     print(f"Error processing {filename}: {e}")
 
 def display_merged_data(merged_file_path):
-    with open(merged_file_path, 'r') as file:
+    with open(merged_file_path, 'r', encoding='utf-8') as file:
         merged_data = file.read()
         st.text_area("Merged Data - in raw txt format", merged_data, height=200)
 
@@ -188,7 +188,7 @@ def sentiment_analysis(merged_file_path):
     nlp = spacy.load('en_core_web_sm')
     nlp.add_pipe('spacytextblob')
 
-    with open(merged_file_path, 'r') as file:
+    with open(merged_file_path, 'r', encoding='utf-8') as file:
         text = file.read()
 
     doc = nlp(text)
